8a9f9025cfc1fafcc4960afcfb7c6947c8cf1155
Nayan Jain <nayanjain1994@gmail.com>
2022-04-16 14:19:35 -0400

    Edit image while editing a post

--- epitomize-frontend/src/Components/EditPost.js
+++ epitomize-frontend/src/Components/EditPost.js
@@ -14,7 +14,9 @@ export default function EditPost() {
     const cookies = new Cookies();
 
     const [dataItem, setData] = useState("")
-    const [file, setFile] = useState("")
+    const [myFile, setFile] = useState("")
+    const [newFile, setNewFile] = useState("")
+
 
     const initialValues = {
         Title: dataItem.Title,
@@ -22,55 +24,69 @@ export default function EditPost() {
         Summary: dataItem.Summary,
         Tags: dataItem.Tags,
         Type: dataItem.Type,
-        File: dataItem.Image,
+        myFile: dataItem.Image,
         LinkedPost: 1,
         Status: 0
     }
+
     const [values, setValues] = useState(initialValues);
 
 
     React.useEffect(() => {
         const tokenStr = cookies.get('access_token')
         let decodedToken = jwt_decode(tokenStr);
-    let currentDate = new Date();
-    if (decodedToken.exp * 1000 < currentDate.getTime()) {
-      cookies.remove("access_token", { path: '/' })
-      window.location = "/"
-    } 
-        axios.get(baseURL + id,  { headers: { "Authorization": `Bearer ${tokenStr}` } })
+        let currentDate = new Date();
+        if (decodedToken.exp * 1000 < currentDate.getTime()) {
+            cookies.remove("access_token", { path: '/' })
+            window.location = "/"
+        }
+        axios.get(baseURL + id, { headers: { "Authorization": `Bearer ${tokenStr}` } })
             .then((response) => {
                 setData(response.data);
-                console.log(response.data)
                 setValues(response.data)
+                setFile(response.data.Image)
             });
     }, []);
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         setValues({
-          ...values,
-          [name]: value,
+            ...values,
+            [name]: value,
         });
-      };
+    };
+
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         const tokenStr = cookies.get('access_token')
-
-        const newPost = values
-        if (file) {
-            const data = new FormData();
-            const filename = Date.now() + file.name;
-            data.append("name", filename);
-            data.append("file", file);
-            //newPost.photo=filename;
-
+        let decodedToken = jwt_decode(tokenStr);
+        let currentDate = new Date();
+        if (decodedToken.exp * 1000 < currentDate.getTime()) {
+            cookies.remove("access_token", { path: '/' })
+            window.location = "/"
         }
+
+        const data = new FormData();
+        data.append("Title", values.Title);
+        data.append("Content", values.Content);
+        data.append("Summary", values.Summary);
+        data.append("Tags", values.Tags);
+        data.append("Type", values.Type);
+        data.append("Status", 0);
+
+        if (newFile !== "") {
+            data.append("myFile", newFile);
+        }
+        else {
+            data.append("myFile", dataItem.Image);
+        }
+
         axios
-            .put(baseURL + id, newPost, { headers: { "Authorization": `Bearer ${tokenStr}` } })
+            .put(baseURL + id, data, { headers: { "Authorization": `Bearer ${tokenStr}` } })
             .then(response => {
                 alert("Post successfully edited.")
-                window.location = '/';
+                window.location = '/myposts';
             }).catch(error => {
                 console.log(error)
             });
@@ -78,8 +94,11 @@ export default function EditPost() {
     return (
         <div className="createpost">
 
-            {file && (
-                <img className="postImg" src={URL.createObjectURL(file)} alt="" />
+            {myFile && (
+                <img className="postImg" src={require("../images/" + dataItem.Image)} alt="" />
+            )}
+            {newFile && (
+                <img className="postImg" src={URL.createObjectURL(newFile)} alt="" />
             )}
             <form className="postForm" onSubmit={handleSubmit}>
                 <div className="postFromGroup">
@@ -91,18 +110,18 @@ export default function EditPost() {
 
                 </div>
                 <div className="postFromGroup">
-                    {/* <input type="file" id="fileInput" style={{ display: "none" }} onChange={(e) => setFile(e.target.files[0])} /> */}
+                    <input type="file" id="fileInput" style={{ display: "none" }} onChange={(e) => setNewFile(e.target.files[0])} />
                     <input type="text" className="postInput" autoFocus={true} name="Title" id="title" data-testid="title"
-                        defaultValue={dataItem.Title} 
+                        defaultValue={dataItem.Title}
                         onChange={handleInputChange}
-                         />
+                    />
 
                 </div>
                 <div className="postFromGroup">
                     <input type="text" placeholder="Summary" className="postSummary" autoFocus={true} id="summary"
                         defaultValue={dataItem.Summary} name="Summary" data-testid="summary"
                         onChange={handleInputChange}
-                         />
+                    />
 
                 </div>
                 <div className="postFromGroup">
@@ -110,18 +129,18 @@ export default function EditPost() {
                 </div>
                 <div className="postFromGroup">
                     <input type="text" placeholder="Tags" className="postTags" autoFocus={true} id="tags"
-                        defaultValue={dataItem.Tags} name="Tags" disabled={true} style={{backgroundColor: "white"}}
+                        defaultValue={dataItem.Tags} name="Tags" disabled={true} style={{ backgroundColor: "white" }}
                         onChange={handleInputChange} data-testid="tags"
-                        />
+                    />
                 </div>
                 <div className="postFromGroup">
-                    <input type="text" placeholder="Type" className="postTags" autoFocus={true}  id="posttype"
+                    <input type="text" placeholder="Type" className="postTags" autoFocus={true} id="posttype"
                         value={dataItem.Type} name="Type" data-testid="posttype"
                         onChange={handleInputChange}
-                         />
+                    />
                 </div>
                 <div className="postFromGroup">
-                    <textarea placeholder="Tell your story.." type="text" className="postInput postText"  id="content"
+                    <textarea placeholder="Tell your story.." type="text" className="postInput postText" id="content"
                         defaultValue={dataItem.Content} name="Content" data-testid="content"
                         onChange={handleInputChange}></textarea>
                 </div>
